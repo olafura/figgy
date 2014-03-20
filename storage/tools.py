0a17908d27e37d52afdcb1da01f55737209f1af5
Olafur Arason <olafura@olafura.com>
2014-03-20 18:56:09 -0400

    Added support for dealing with wrong id in books

--- storage/tools.py
+++ storage/tools.py
@@ -18,11 +18,25 @@ def process_book_element(book_element):
     book, created = Book.objects.get_or_create(pk=book_element.get('id'))
     book.title = book_element.findtext('title')
     book.description = book_element.findtext('description')
+    aliases = {}
+    same_aliases = False
+    book_aliases = {}
+    for alias in book.aliases.values():
+        book_aliases[alias['value']] = True
 
     for alias in book_element.xpath('aliases/alias'):
         scheme = alias.get('scheme')
         value = alias.get('value')
+        aliases[scheme] = value
+        if value in book_aliases:
+            same_aliases = True
 
+    if same_aliases == False and len(book_aliases) > 0:
+        book, created = Book.objects.get_or_create(pk=aliases.values()[0])
+        book.title = book_element.findtext('title')
+        book.description = book_element.findtext('description')
+
+    for scheme, value in aliases.items():
         try:
             book.aliases.get_or_create(scheme=scheme, value=value)
         except IntegrityError as e:
